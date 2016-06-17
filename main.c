082d6c96e2b191ca86e52d12146f0a248671da4a
Florian Lecoeuche <lecoeuche.florian@gmail.com>
2016-06-17 20:11:57 +0200

    Some rework and it ROCKS !

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "donnees.h"
+//#include "donnees.h"
 #include "hdd.h"
 
 
@@ -11,40 +11,40 @@ int main()
     FICHIER fic1;
     fic1.nom="notes.txt";
 
-    dd.taille=64000;        // Taille du disque
-    dd.tabBlock[64000];   // Déclaration tableau de pointeurs sur BLOCK en fonction de la taille du DD
+//    dd.taille=64000;        // Taille du disque
+//    dd.tabBlock[64000];   // Déclaration tableau de pointeurs sur BLOCK en fonction de la taille du DD
     unsigned int taille;
-    taille=dd.taille;
+    taille=64000;
 
 
-    // initBlockTab(dd,taille); // Fonction init tableau de blocs
+    dd = initBlockTab(taille); // Fonction init tableau de blocs
 
-    for(i=0; i<taille; i++)
-    {
+//    for(i=0; i<taille; i++)
+//    {
+//
+//        BLOCK *monBloc;
+//        monBloc = malloc(sizeof(BLOCK));
+//        dd.tabBlock[i]=monBloc;                                 // Association du Block avec sa case dans le tableau de Blocks
+//        dd.tabBlock[i]->numero=i;                               // Initialisation des numéros de blocks de 0 à Taille du DD -1
+//        dd.tabBlock[i]->etat=0;
+//        printf("Bloc numéro : %d\n", dd.tabBlock[i]->numero);
+//        printf("Fichier du bloc %d : %s\n",i,dd.tabBlock[i]->fichier.nom);
+//        printf("Etat du bloc : %d\n",dd.tabBlock[i]->etat);
+//        dd.tabBlock[i]=monBloc;
+//
+//    }
+//    printf("Les %d blocs ont bien été initialisés.\n",taille);
 
-        BLOCK *monBloc;
-        monBloc = malloc(sizeof(BLOCK));
-        dd.tabBlock[i]=monBloc;                                 // Association du Block avec sa case dans le tableau de Blocks
-        dd.tabBlock[i]->numero=i;                               // Initialisation des numéros de blocks de 0 à Taille du DD -1
-        dd.tabBlock[i]->etat=0;
-        printf("Bloc numéro : %d\n", dd.tabBlock[i]->numero);
-        printf("Fichier du bloc %d : %s\n",i,dd.tabBlock[i]->fichier.nom);
-        printf("Etat du bloc : %d\n",dd.tabBlock[i]->etat);
-        dd.tabBlock[i]=monBloc;
+    dd.tabBlock[0].fichier=fic1;
+    dd.tabBlock[0].etat=1;
+    printf("Fichier du bloc : %s\n",dd.tabBlock[0].fichier.nom);
+    printf("Etat du bloc : %d\n",dd.tabBlock[0].etat);
 
-    }
-    printf("Les %d blocs ont bien été initialisés.\n",taille);
-
-    dd.tabBlock[0]->fichier=fic1;
-    dd.tabBlock[0]->etat=1;
-    printf("Fichier du bloc : %s\n",dd.tabBlock[0]->fichier.nom);
-    printf("Etat du bloc : %d\n",dd.tabBlock[0]->etat);
-
-    for(i=0;i<1024;i++)
-    {
-        fic1.donnees[i]="Data";
-        printf("Case %d %s\n",i,fic1.donnees[i]);
-    }
+//    for(i=0;i<1024;i++)
+//    {
+//        fic1.donnees[i]="Data";
+//        printf("Case %d %s\n",i,fic1.donnees[i]);
+//    }
 
 
 
