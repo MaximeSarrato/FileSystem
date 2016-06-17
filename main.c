01a2cc0abb708a69a4258efdb3f9aff62cf2c1f9
MaximeSto <maxime.sarrato@gmail.com>
2016-06-17 23:41:57 +0200

    Remove comments and add gitignore

--- main.c
+++ main.c
@@ -9,31 +9,12 @@ int main()
     HARD_DISK dd; // Déclaration de mon DD
     int i;
     FICHIER fic1;
-    fic1.nom="notes.txt";
-
-//    dd.taille=64000;        // Taille du disque
-//    dd.tabBlock[64000];   // Déclaration tableau de pointeurs sur BLOCK en fonction de la taille du DD
     unsigned int taille;
     taille=64000;
-
+    fic1.nom="notes.txt";
 
     dd = initBlockTab(taille); // Fonction init tableau de blocs
 
-//    for(i=0; i<taille; i++)
-//    {
-//
-//        BLOCK *monBloc;
-//        monBloc = malloc(sizeof(BLOCK));
-//        dd.tabBlock[i]=monBloc;                                 // Association du Block avec sa case dans le tableau de Blocks
-//        dd.tabBlock[i]->numero=i;                               // Initialisation des numéros de blocks de 0 à Taille du DD -1
-//        dd.tabBlock[i]->etat=0;
-//        printf("Bloc numéro : %d\n", dd.tabBlock[i]->numero);
-//        printf("Fichier du bloc %d : %s\n",i,dd.tabBlock[i]->fichier.nom);
-//        printf("Etat du bloc : %d\n",dd.tabBlock[i]->etat);
-//        dd.tabBlock[i]=monBloc;
-//
-//    }
-//    printf("Les %d blocs ont bien été initialisés.\n",taille);
 
     dd.tabBlock[0].fichier=fic1;
     dd.tabBlock[0].etat=1;
